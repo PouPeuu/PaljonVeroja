8e3c0a639d9d8753b34db5f9446394e3e3541a32
PouPeuu <qwertydude277@gmail.com>
2024-08-13 06:25:14 +0300

    Fyyngplööhil Funkfaarnu

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@
 int main(int argc, char *argv[]) {
 	std::srand(std::time(0));
 
-	uint amount = std::stoi(argv[1]);
-	for(uint i=0;i<amount;++i) {
-		std::cout << Names::create_name() << " " << Names::create_name() << std::endl;
+	if (argc < 2) {
+		std::cout << "Kill yourself\n";
+	} else {
+		uint amount = std::stoi(argv[1]);
+		for(uint i=0;i<amount;++i) {
+			std::cout << Names::create_name() << " " << Names::create_name() << std::endl;
+		}
 	}
 	return 0;
 }
--- names.cpp
+++ names.cpp
@@ -1,14 +1,16 @@
 #include "names.h"
 
-std::vector<std::string> consonants = {"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "ng"};
+std::vector<std::string> consonants = {"d", "f", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v"};
+std::vector<std::string> sonorants = {"m", "n", "ng", "r", "l"};
+std::vector<std::string> dentals = {"s", "t", "n", "r", "l"};
 
 std::vector<std::string> back_vowels = {"a", "o", "u"};
 std::vector<std::string> neutral_vowels = {"e", "i"};
 std::vector<std::string> front_vowels = {"ä", "ö", "y"};
 
-std::vector<std::string> back_diphthongs = {"ai", "oi", "ui", "au", "ou", "eu", "iu", "uo", "ey"};
+std::vector<std::string> back_diphthongs = {"ai", "oi", "ui", "au", "ou", "eu", "iu", "uo"};
 std::vector<std::string> neutral_diphthongs = {"ei", "ie"};
-std::vector<std::string> front_diphthongs = {"äi", "öi", "yi", "äy", "öy", "yö"};
+std::vector<std::string> front_diphthongs = {"äi", "öi", "yi", "äy", "öy", "yö", "ey"};
 
 std::vector<std::string> back_doubled = {"aa", "oo", "uu"};
 std::vector<std::string> neutral_doubled = {"ee", "ii"};
@@ -49,56 +51,50 @@ std::unordered_map<VowelType, std::vector<std::string>> backs = {
 	{DOUBLE, back_doubled}
 };
 
-std::string create_syllable(WordPos word_pos, bool firstk, bool endk, bool front_mode) {
-	std::vector<std::string> initial_consonants;
-	std::vector<std::string> final_consonants;
-	switch(word_pos) {
-		case INITIAL:
-			initial_consonants = word_initial_consonants;
-			final_consonants = Utils::merge(obstruents, liquids);
-			break;
-		case MEDIAL:
-			initial_consonants = Utils::merge(obstruents, liquids);
-			final_consonants = Utils::merge(obstruents, liquids);
-			break;
-		case FINAL:
-			initial_consonants = Utils::merge(obstruents, liquids);
-			final_consonants = coronals;
-			break;
-	}
-
-	std::string syllable = "";
-
-	if (firstk) {
-		syllable += initial_consonants[std::rand() % initial_consonants.size()];
-	}
-
-
+std::string create_vowel(bool front_mode) {
+	std::string vowel;
+	std::vector<std::string> possible_vowels;
 	VowelType vowel_type;
-	int random_number = std::rand() % 4;
 
-	if (random_number >= 2) {
+	int dide_nesi = std::rand() % 4;
+
+	if (dide_nesi >= 2) {
 		vowel_type = SINGLE;
-	} else if (random_number == 1) {
+	} else if (dide_nesi == 1) {
 		vowel_type = DIPHTHONG;
 	} else {
 		vowel_type = DOUBLE;
 	}
 
-	std::vector<std::string> vowel_sequences;
-	
-	vowel_sequences = Utils::merge(vowel_sequences, neutrals[vowel_type]);
+	possible_vowels = Utils::merge(possible_vowels, neutrals[vowel_type]);
 
 	if (front_mode) {
-		vowel_sequences = Utils::merge(vowel_sequences, fronts[vowel_type]);
+		possible_vowels = Utils::merge(possible_vowels, fronts[vowel_type]);
 	} else {
-		vowel_sequences = Utils::merge(vowel_sequences, backs[vowel_type]);
+		possible_vowels = Utils::merge(possible_vowels, backs[vowel_type]);
 	}
 
-	syllable += vowel_sequences[std::rand() % vowel_sequences.size()];
+	return possible_vowels[std::rand() % possible_vowels.size()];
+}
 
-	if (endk) {
-		syllable += final_consonants[std::rand() % final_consonants.size()];
+std::string create_syllable(WordPos word_pos, bool firstc, bool mids, bool endk, bool endd, bool front_mode) {
+	std::string syllable = "";
+
+	switch(word_pos) {
+		case MEDIAL:
+			if (firstc)
+				syllable += consonants[std::rand() % consonants.size()];
+			syllable += create_vowel(front_mode);
+			if (mids)
+				syllable += sonorants[std::rand() % sonorants.size()];
+			if (endk)
+				syllable += consonants[std::rand() % consonants.size()];
+		case FINAL:
+			if (firstc)
+				syllable += consonants[std::rand() % consonants.size()];
+			syllable += create_vowel(front_mode);
+			if (endd)
+				syllable += dentals[std::rand() % dentals.size()];
 	}
 
 	return syllable;
@@ -109,11 +105,11 @@ std::string Names::create_name(bool uppercase_first) {
 
 	bool front_mode = Utils::randbool();
 
-	name += create_syllable(INITIAL, true, Utils::randbool(), front_mode);
-	for (int i = 0; i < std::rand() % 2; ++i) {
-		name += create_syllable(MEDIAL, true, Utils::randbool(), front_mode);
+	name += create_syllable(MEDIAL, true, Utils::randbool(), Utils::randbool(), Utils::randbool(), front_mode);
+	for (int i = 0; i < std::rand() % 1; ++i) {
+		name += create_syllable(MEDIAL, true, Utils::randbool(), Utils::randbool(), Utils::randbool(), front_mode);
 	}
-	name += create_syllable(FINAL, true, Utils::randbool(), front_mode);
+	name += create_syllable(FINAL, true, false, false, Utils::randbool(), front_mode);
 
 	name[0] = toupper(name[0]);
 	return name;

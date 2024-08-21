36af22f6cde3f690d3e4940e9879b28a04b2fb22
PouPeuu <qwertydude277@gmail.com>
2024-08-21 12:26:26 +0300

    Denest CSV

--- CSV.cpp
+++ CSV.cpp
@@ -1,5 +1,46 @@
 #include "CSV.h"
 
+std::vector<std::string> process_line(std::string line) {
+	std::vector<std::string> line_result;
+	std::string current = "";
+
+	bool inside_quotes = false;
+	bool escape = false;
+
+	for (size_t i = 0; i < line.size(); ++i) {
+		char c = line[i];
+
+		if (escape) {
+			current += c;
+			escape = false;
+			continue;
+		}
+
+		if (!inside_quotes && c == ',') {
+			line_result.push_back(current);
+			current.clear();
+			continue;
+		}
+
+		if (c == '\\') {
+			escape = true;
+			continue;
+		}
+
+		if (c == '"') {
+			inside_quotes = !inside_quotes;
+			continue;
+		}
+
+		current += c;
+	}
+
+	if (!current.empty()) {
+		line_result.push_back(current);
+	}
+	
+	return line_result;
+}
 
 std::vector<std::vector<std::string>> CSV::load_csv(fs::path filepath) {
 	std::vector<std::vector<std::string>> result;
@@ -10,37 +51,7 @@ std::vector<std::vector<std::string>> CSV::load_csv(fs::path filepath) {
 	assert(file.is_open());
 
 	while (std::getline(file, line)) {
-		std::vector<std::string> line_result;
-
-		std::string current = "";
-		bool inside_quotes = false;
-		bool escape = false;
-
-		for (size_t i = 0; i < line.size(); ++i) {
-			char c = line[i];
-
-			if ((!inside_quotes && c == ',')) {
-				line_result.push_back(current);
-				current.clear();
-			} else if (!escape) {
-				if (c == '"') {
-					inside_quotes = !inside_quotes;
-				} else if (c == '\\') {
-					escape = true;
-				} else {
-					current += c;
-				}
-			} else {
-				current += c;
-				escape = false;
-			}
-
-			if(i == line.size() - 1) {
-				line_result.push_back(current);
-			}
-		}
-
-		result.push_back(line_result);
+		result.push_back(process_line(line));
 	}
 
 	file.close();
